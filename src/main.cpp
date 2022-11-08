d401e13df600fab080a11dbc20b9475a5a934a1f
AdrielWippel <adriel.wippel@outlook.com>
2022-11-08 11:25:08 -0300

    add

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+  delay(1);
+}
+
+void loop() {
+
+  int i = 0;
+  Serial.println("Início ");
+  for (i = 0; i < 30; i++)
+  {
+  digitalWrite(2, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  delay(200);
+  Serial.println("Timer");
+  }
+  
+  Serial.println("Sleep ");
+  delay(1);
+  ESP.deepSleep(120e6);
+}
